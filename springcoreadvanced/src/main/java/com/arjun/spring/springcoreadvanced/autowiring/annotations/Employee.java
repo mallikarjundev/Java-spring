423ef44eda905f3cd70528fa5ccce08d97741d74
arjunoggu <mallikarjundev2@gmail.com>
2023-05-13 13:52:18 -0400

    Adds spring core advanced

---
+++ springcoreadvanced/src/main/java/com/arjun/spring/springcoreadvanced/autowiring/annotations/Employee.java
@@ -0,0 +1,29 @@
+package com.arjun.spring.springcoreadvanced.autowiring.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Employee {
+
+	private Address address;
+
+	Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Autowired
+	@Qualifier("address123")
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+}
---
+++ springcoreadvanced/src/main/java/com/arjun/spring/springcoreadvanced/standalone/collections/Test.java
@@ -0,0 +1,16 @@
+package com.arjun.spring.springcoreadvanced.standalone.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/arjun/spring/springcoreadvanced/standalone/collections/config.xml");
+		ProductsList productsList = (ProductsList) context.getBean("productsList");
+		System.out.println(productsList);
+	}
+
+}
