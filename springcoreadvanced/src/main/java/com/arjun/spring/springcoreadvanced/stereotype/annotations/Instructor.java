7f4c778404f2b2aa24f50ff6ee6d7573d199ab8e
arjunoggu <mallikarjundev2@gmail.com>
2023-05-14 19:19:33 -0400

    creating objects inside annotations

--- springcoreadvanced/src/main/java/com/arjun/spring/springcoreadvanced/stereotype/annotations/Instructor.java
+++ springcoreadvanced/src/main/java/com/arjun/spring/springcoreadvanced/stereotype/annotations/Instructor.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class Instructor {
 
-	@Value("#{T(java.lang.Math).abs(-12)}")
+	@Value("#{new Integer(21)}")
 	int id;
 	@Value("Arjun O")
 	String name;
