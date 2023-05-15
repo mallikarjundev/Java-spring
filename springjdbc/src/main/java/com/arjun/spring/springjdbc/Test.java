14042b010a58332b8f932ccfb8347766044e2f64
arjunoggu <mallikarjundev2@gmail.com>
2023-05-15 17:51:04 -0400

    tested jdbc template and success

---
+++ springjdbc/src/main/java/com/arjun/spring/springjdbc/Test.java
@@ -0,0 +1,18 @@
+package com.arjun.spring.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/arjun/spring/springjdbc/config.xml");
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
+		String sql = "insert into employee values(?,?,?)";
+		int result = jdbcTemplate.update(sql, new Integer(1), "Arjun", "o");
+		System.out.println("Number of records inserted are: " + result);
+	}
+
+}
