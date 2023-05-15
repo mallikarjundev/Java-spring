53b8c56e556873e7980055f3acd3b88945f3dbae
arjunoggu <mallikarjundev2@gmail.com>
2023-05-15 18:52:49 -0400

    created and update test class(success) & added one row.

--- springjdbc/src/main/java/com/arjun/spring/springjdbc/employee/test/Test.java
+++ springjdbc/src/main/java/com/arjun/spring/springjdbc/employee/test/Test.java
@@ -2,16 +2,22 @@ package com.arjun.spring.springjdbc.employee.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.arjun.spring.springjdbc.employee.dao.EmployeeDao;
+import com.arjun.spring.springjdbc.employee.dto.Employee;
 
 public class Test {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/arjun/spring/springjdbc/config.xml");
-		JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
-		String sql = "insert into employee values(?,?,?)";
-		int result = jdbcTemplate.update(sql, new Integer(1), "Arjun", "o");
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/arjun/spring/springjdbc/employee/test/config.xml");
+		EmployeeDao dao = (EmployeeDao) context.getBean("employeeDao");
+		Employee employee = new Employee();
+		employee.setId(2);
+		employee.setFirstName("John");
+		employee.setLastName("B");
+		int result = dao.create(employee);
 		System.out.println("Number of records inserted are: " + result);
 	}
 
