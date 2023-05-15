9b7b4b00ac3248a9f07d63d34ff0bb691f40abe8
arjunoggu <mallikarjundev2@gmail.com>
2023-05-15 18:38:10 -0400

    created employee class, dao interface and wired jdbc template to daoimpl

---
+++ springjdbc/src/main/java/com/arjun/spring/springjdbc/employee/dao/EmployeeDao.java
@@ -0,0 +1,8 @@
+package com.arjun.spring.springjdbc.employee.dao;
+
+import com.arjun.spring.springjdbc.employee.dto.Employee;
+
+public interface EmployeeDao {
+
+	int create(Employee employee);
+}
