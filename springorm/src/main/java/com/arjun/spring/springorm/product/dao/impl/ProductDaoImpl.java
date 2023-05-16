cbd2a56dff0389a91c764800158b76168f5b3fb2
arjunoggu <mallikarjundev2@gmail.com>
2023-05-16 18:48:55 -0400

    autowired

--- springorm/src/main/java/com/arjun/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/arjun/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,12 +1,16 @@
 package com.arjun.spring.springorm.product.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.arjun.spring.springorm.product.dao.ProductDao;
 import com.arjun.spring.springorm.product.entity.Product;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
+	@Autowired
 	HibernateTemplate hibernateTemplate;
 
 	@Override
