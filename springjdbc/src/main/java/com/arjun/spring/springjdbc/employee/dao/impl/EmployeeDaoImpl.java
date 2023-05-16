8e2b87b4406df4e951453726044540b1c0301154
arjunoggu <mallikarjundev2@gmail.com>
2023-05-15 20:29:03 -0400

    autowired annotation used

--- springjdbc/src/main/java/com/arjun/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
+++ springjdbc/src/main/java/com/arjun/spring/springjdbc/employee/dao/impl/EmployeeDaoImpl.java
@@ -2,14 +2,18 @@ package com.arjun.spring.springjdbc.employee.dao.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import com.arjun.spring.springjdbc.employee.dao.EmployeeDao;
 import com.arjun.spring.springjdbc.employee.dao.rowmapper.EmployeeRowMapper;
 import com.arjun.spring.springjdbc.employee.dto.Employee;
 
+@Component("employeeDao")
 public class EmployeeDaoImpl implements EmployeeDao {
 
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
