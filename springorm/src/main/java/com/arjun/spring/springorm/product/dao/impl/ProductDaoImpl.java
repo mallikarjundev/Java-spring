7f921ecac8aceafc25b614dc7f181bc42b7942d3
arjunoggu <mallikarjundev2@gmail.com>
2023-05-16 19:01:17 -0400

    updated create method in impl class and added transactinal annotation

--- springorm/src/main/java/com/arjun/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/arjun/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,8 +1,11 @@
 package com.arjun.spring.springorm.product.dao.impl;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.arjun.spring.springorm.product.dao.ProductDao;
 import com.arjun.spring.springorm.product.entity.Product;
@@ -14,9 +17,10 @@ public class ProductDaoImpl implements ProductDao {
 	HibernateTemplate hibernateTemplate;
 
 	@Override
+	@Transactional
 	public int create(Product product) {
-
-		return 0;
+		Integer result = (Integer) hibernateTemplate.save(product);
+		return result;
 	}
 
 }
