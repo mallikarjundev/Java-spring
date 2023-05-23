7da586db77c34f64127ef78740a53cdcf2861abd
arjunoggu <mallikarjundev2@gmail.com>
2023-05-23 10:48:15 -0400

    migrated app. used model

--- springmvc/src/main/java/com/arjun/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/arjun/spring/springmvc/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.arjun.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.arjun.spring.springmvc.dto.User;
 
@@ -12,19 +12,32 @@ import com.arjun.spring.springmvc.dto.User;
 public class UserController {
 
 	@RequestMapping("registrationPage")
-	public ModelAndView showRegistrationPage() {
-
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("userReg");
-		return modelAndView;
+	public String showRegistrationPage() {
+		return "userReg";
 	}
+
+//	@RequestMapping("registrationPage")
+//	public ModelAndView showRegistrationPage() {
+//
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.setViewName("userReg");
+//		return modelAndView;
+//	}
 
 	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
+
 		System.out.println(user);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("regResult");
-		return modelAndView;
+		model.addAttribute("user", user);
+		return "regResult";
 	}
+
+//	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
+//	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+//		System.out.println(user);
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.addObject("user", user);
+//		modelAndView.setViewName("regResult");
+//		return modelAndView;
+//	}
 }
