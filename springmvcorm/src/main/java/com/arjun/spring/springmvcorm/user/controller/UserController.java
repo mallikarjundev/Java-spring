5bfeb456840982cfd71e6bfc4c724653e3dcff06
arjunoggu <mallikarjundev2@gmail.com>
2023-05-23 14:49:59 -0400

    added method get data from form and send it to db and returning result in same page.

--- springmvcorm/src/main/java/com/arjun/spring/springmvcorm/user/controller/UserController.java
+++ springmvcorm/src/main/java/com/arjun/spring/springmvcorm/user/controller/UserController.java
@@ -2,7 +2,12 @@ package com.arjun.spring.springmvcorm.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.arjun.spring.springmvcorm.user.entity.User;
 import com.arjun.spring.springmvcorm.user.servies.UserService;
 
 @Controller
@@ -10,6 +15,20 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
+
+	@RequestMapping("registrationPage")
+	public String showRegistrationPage() {
+		return "userReg";
+	}
+
+	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
+
+		int result = service.save(user);
+		model.addAttribute("result", "user created with Id" + result);
+
+		return "userReg";
+	}
 
 	public UserService getService() {
 		return service;
