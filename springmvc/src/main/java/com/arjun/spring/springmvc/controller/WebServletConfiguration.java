aa2828b87c36f0c6b08b499a30d615ba00b24aa6
arjunoggu <mallikarjundev2@gmail.com>
2023-05-31 00:57:12 -0400

    added dispatcher and mapping

--- springmvc/src/main/java/com/arjun/spring/springmvc/controller/WebServletConfiguration.java
+++ springmvc/src/main/java/com/arjun/spring/springmvc/controller/WebServletConfiguration.java
@@ -2,9 +2,11 @@ package com.arjun.spring.springmvc.controller;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 
 public class WebServletConfiguration implements WebApplicationInitializer {
 
@@ -13,6 +15,10 @@ public class WebServletConfiguration implements WebApplicationInitializer {
 
 		AnnotationConfigWebApplicationContext webcontext = new AnnotationConfigWebApplicationContext();
 		webcontext.register(SpringConfig.class);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet());
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
 	}
 
 }
