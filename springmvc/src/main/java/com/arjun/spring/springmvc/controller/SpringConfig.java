f9215e4fb916a85e3f5110656de9f29541213de9
arjunoggu <mallikarjundev2@gmail.com>
2023-05-31 00:49:57 -0400

    added default config handler

--- springmvc/src/main/java/com/arjun/spring/springmvc/controller/SpringConfig.java
+++ springmvc/src/main/java/com/arjun/spring/springmvc/controller/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,5 +20,10 @@ public class SpringConfig extends WebMvcConfigurationSupport {
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+
+	@Override
+	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
 	}
 }
