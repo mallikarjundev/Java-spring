2d8e6138ec390d1e2dcc9dfdf5802c2270dd91d0
arjunoggu <mallikarjundev2@gmail.com>
2023-05-30 23:56:54 -0400

    updated test

--- springjavaconfig/src/main/java/com/arjun/spring/springjavaconfig/Test.java
+++ springjavaconfig/src/main/java/com/arjun/spring/springjavaconfig/Test.java
@@ -8,8 +8,8 @@ public class Test {
 	public static void main(String[] args) {
 
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-		Dao dao = context.getBean(Dao.class);
-		dao.create();
+		Service service = context.getBean(Service.class);
+		service.save();
 	}
 
 }
