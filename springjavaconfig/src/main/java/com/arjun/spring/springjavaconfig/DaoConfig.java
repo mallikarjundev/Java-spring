cebdee17ca0350762aa770d81a29688dc80d588a
arjunoggu <mallikarjundev2@gmail.com>
2023-05-31 00:00:05 -0400

    created separate daoconfig file and separated from main config

--- springjavaconfig/src/main/java/com/arjun/spring/springjavaconfig/DaoConfig.java
+++ springjavaconfig/src/main/java/com/arjun/spring/springjavaconfig/DaoConfig.java
@@ -2,14 +2,12 @@ package com.arjun.spring.springjavaconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(DaoConfig.class)
-public class SpringConfig {
+public class DaoConfig {
 
 	@Bean
-	public Service service() {
-		return new Service();
+	public Dao dao() {
+		return new Dao();
 	}
 }
