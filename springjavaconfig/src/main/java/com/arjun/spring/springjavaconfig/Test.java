e38563d659fd397273c5003c7465417265afb89a
arjunoggu <mallikarjundev2@gmail.com>
2023-05-30 23:51:56 -0400

    created and tested test class

---
+++ springjavaconfig/src/main/java/com/arjun/spring/springjavaconfig/Test.java
@@ -0,0 +1,15 @@
+package com.arjun.spring.springjavaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+		Dao dao = context.getBean(Dao.class);
+		dao.create();
+	}
+
+}
